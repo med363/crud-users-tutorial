c4a717e2a7de01ec77231121b185be626fc421b8
med amine blibech <56481634+med363@users.noreply.github.com>
2022-04-19 12:21:42 +0200

    Update users.controller.js

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,16 +1,21 @@
 const Users = require("../models/users.models");
 const ValidateUser = require("../validation/Users.validation");
+//tout les fcts doivent etre async pourlaquelle les requettes est bien aboutissent et dans ces fcts on import le nom de model ==> users dans le dossier models e fichiers users.models.js
 const AddUser = async (req, res) => {
   const { errors, isValid } = ValidateUser(req.body);
   try {
     if (!isValid) {
+      /*errors c'est un objet contient tout les errs de la validation dans le dossieer validate*/
       res.status(404).json(errors);
     } else {
+      /*validation sur l'email if mail exist status404 ce mecame de validation se trouve dans le dossier validation 1.validate if utilise des espaces akahaw dans le fichier isEmpty*/
       await Users.findOne({ Email: req.body.Email }).then(async (exist) => {
         if (exist) {
+          /*on stocke cette reponse dans l'objet errors*/
           errors.Email = "User Exist";
           res.status(404).json(errors);
         } else {
+          /*on ajout user avec la methode create ou save à partir min body*/
           await Users.create(req.body);
           res.status(201).json({ message: "User added with success" });
         }
@@ -65,7 +70,7 @@ const DeleteUser = async (req, res) => {
     console.log(error.message);
   }
 };
-
+//export tout les fcts pour n peut les import dans route
 module.exports = {
   AddUser,
   FindAllUsers,
@@ -73,3 +78,4 @@ module.exports = {
   UpdateUser,
   DeleteUser,
 };
+/*concurently npm pour demarrer les deux  servers en meme temps*/
